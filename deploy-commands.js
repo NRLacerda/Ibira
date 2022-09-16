90a43127ce765e6686486f25074dcc8d5f9873ad
NRLacerda <nrochalacerda@gmail.com>
2022-09-16 16:18:02 -0300

    master add, funcoes novas

--- deploy-commands.js
+++ deploy-commands.js
@@ -4,9 +4,17 @@ const { clientId, guildId, token } = require('./config.json'); //puxa as info do
 
 
 // Declaração dos comandos e suas descrições
+// Pelo amor de deus encontrar uma forma menos feia de fazer isso, talvez por import em Typescript
 const commands = 
 [
-	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
+	new SlashCommandBuilder().setName('server').setDescription('Infos do server'),
+    new SlashCommandBuilder().setName('ajuda').setDescription('Precisa de ajuda gafanhoto?'),
+    new SlashCommandBuilder().setName('java').setDescription('Java  !Javascript'),
+    new SlashCommandBuilder().setName('javascript').setDescription('Javascript ! Java'),
+    new SlashCommandBuilder().setName('sql').setDescription('um banco... de dados?'),
+    new SlashCommandBuilder().setName('htmlcss').setDescription('<h1>HTML/CSS</h1>'),
+    new SlashCommandBuilder().setName('bot').setDescription('01100010 01101111 01110100'),
+    new SlashCommandBuilder().setName('wrap').setDescription('Wrapando tudo!')
 ]
 
 .map(command => command.toJSON());
@@ -17,3 +25,6 @@ const rest = new REST({ version: '10' }).setToken(token);
 rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
 	.then((data) => console.log(`Successfully registered ${data.length} application commands.`))
 	.catch(console.error);
+    
+// Rodar apenas pra add novas features
+// node deploy-commands.js
--- index.js
+++ index.js
@@ -10,7 +10,7 @@ client.once('ready', () => {
 	console.log('Life goes on and on and on');
 });
 
-// Tentando trocar o prefixo do BOT
+// Tentando trocar o prefixo do BO, Spoiler: não deu certo
 /*
 const prefix = '+'
 
@@ -34,10 +34,24 @@ client.on('interactionCreate', async interaction => {
     if (!interaction.isChatInputCommand()) return;
 
     const { commandName } = interaction;
-
+// futuramente separar essas interações em pastas pra ser menos feio e um código mais clean.
     if (commandName === 'server') 
     { 
-        await interaction.reply(`:green_circle: Ibira notou que o server se chama:\n **${interaction.guild.name}** ! \n e foi criado em "${interaction.guild.createdAt}"`);
+        await interaction.reply(`:green_circle: **Ibira** notou que o server se chama:\n **${interaction.guild.name}** !\nFoi criado em "${interaction.guild.createdAt}"`);
+    }else if (commandName === 'javascript'){
+        await interaction.reply(':green_circle: **Ibira** disse que existem livros sagrados que podem te dizer mais sobre isso... \n MDN = https://developer.mozilla.org/pt-BR/docs/Web/JavaScript');
+    }else if (commandName === 'ajuda') {
+        await interaction.reply(':green_circle: **Ibira** tem os seguintes métodos de invocação:\n"/ajuda", "/server,"/sql","/javascript","/htmlcss"');     
+    }else if (commandName === 'sql'){
+        await interaction.reply(':green_circle: **SQL**? é uma linguagem de programação utilizada amplamente em Banco de Dados, baseado em "Querys",\nUma linguagem fácil de programar!\nVeja sobre em: https://blog.betrybe.com/sql/')
+    }else if (commandName === 'htmlcss'){
+        await interaction.reply(':green_circle: **HTML/CSS** é por onde todos começam geralmente, existe uma quantidade de informação sobre front infinta na internet!\n Da uma olhada: \nSobre HTML: https://developer.mozilla.org/pt-BR/docs/Web/HTML \nSobre CSS: https://developer.mozilla.org/pt-BR/docs/Web/CSS')
+    }else if (commandName === 'bot'){
+        await interaction.reply(':green_circle: Sobre **01100101 01110101**?\nFui criado em 16/09/22, pela **Organização**,\nMais especificamente pelo organizador Nicolas Cavalcanti\ncom o propósito de guiar no mundo da programação!\nTem alguma sugestão de melhoria, ou correção de bug? Manda uma DM!')
+    }else if (commandName === 'nocode'){
+        await interaction.reply(':green_circle: Sem código mano? ai é dificil de ajudar você\nPosta seu código aqui wrapado!\nNão sabe o que é "wrap"? Use /wrap')
+    }else if (commandName == 'wrap'){
+        await interaction.reply(':green_circle: Wrapar é colocar o código num formato e cores da linguagem que você escreveu.\nDessa forma \n"js\n Seu código aqui" \nResultado->\n ```js\nfunction Exemplo(){\nwindow.alert("onde está js coloque a linguagem que você quer, e na linha de baixo o teu código!")}```')
     }
     /* 
     else if (commandName === 'jsfun') {
